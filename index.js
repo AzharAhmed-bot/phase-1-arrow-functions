c1d71b94834f51e15c89b8f76d2f0432c5e8ba72
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-03-14 22:03:12 +0300

    Complete assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide=function(){
+    let division=2000/100;
+    return division;
+}
+console.log(divide);
+const square = (number) => {
+    let mult = number * number;
+    return mult;
+}
+console.log(square())
+const add=(num1,num2) =>{
+   let  addition=num1+num2;
+    return addition;
+}
+console.log(add());
